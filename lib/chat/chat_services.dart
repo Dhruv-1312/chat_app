2fbdc761bffb337e8071c529a77b898395d5f23c
Dhruv Dhall <117975448+Dhruv-1312@users.noreply.github.com>
2023-08-26 13:32:48 +0530

    chatServices

---
+++ lib/chat/chat_services.dart
@@ -0,0 +1,39 @@
+import 'package:chat_app/models/message.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatServices extends ChangeNotifier {
+  Future<void> sendMessage(String receiverid, String message) async {
+    final String currentuseruid = FirebaseAuth.instance.currentUser!.uid;
+    final Timestamp timestamp = Timestamp.now();
+
+    Message newmessage = Message(
+        senderid: currentuseruid,
+        receiverid: receiverid,
+        message: message,
+        timestamp: timestamp);
+
+    List<String> ids = [currentuseruid, receiverid];
+    ids.sort();
+    String Chatroomid = ids.join('_');
+
+    await FirebaseFirestore.instance
+        .collection('ChatRoom')
+        .doc(Chatroomid)
+        .collection('Messages')
+        .add(newmessage.toJason());
+
+    Stream<QuerySnapshot> getMessage(String userid, String otheruserid) {
+      List<String> ids = [userid, otheruserid];
+      ids.sort();
+      String roomid = ids.join('_');
+      return FirebaseFirestore.instance
+          .collection('ChatRoom')
+          .doc(roomid)
+          .collection('Messages')
+          .orderBy('timestamp', descending: false)
+          .snapshots();
+    }
+  }
+}
